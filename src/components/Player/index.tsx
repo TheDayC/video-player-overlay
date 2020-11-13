3ef6337dec8564584e9d289fed93ffd6582dd27f
TheDayC <daveconnolly90@gmail.com>
2020-11-13 16:28:46 +0000

    Added the video player

---
+++ src/components/Player/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const url = 'https://buffup-public.s3.eu-west-2.amazonaws.com/video/toronto+nba+cut+3.mp4';
+
+const Player: React.FC = () => {
+    return (
+        <div className="playerWrapper">
+            <video controls>
+                <source src={url} type="video/mp4" />
+                Please switch to a browser that supports HTML 5.
+            </video>
+        </div>
+    );
+};
+
+export default Player;
